fb552c8c6599e6a5be4412191279aafd86a969f1
Lou Leone <louis.leone@gmail.com>
2012-02-03 13:37:42 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LouApp::Application.config.secret_token = 'b1ebc8310bc81e58f7238f8427177d569c5de415e6cb1fe51c39c423b1dc64688a326572f073f13f712bd03d2607e0e317f9a1ae50e4cfbff15ca2a5f55db1ac'
